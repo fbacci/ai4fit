eb254a725c41e38cfc98aa1aae6397840db7c809
frensis <bacci.francesca@live.it>
2019-03-20 10:28:51 +0100

    urls.py

--- ai4fit/urls.py
+++ ai4fit/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('ai4fitapp/', include('ai4fitapp.urls')),
+    path('', include('ai4fitapp.urls')),
     path('admin/', admin.site.urls),
 ]
